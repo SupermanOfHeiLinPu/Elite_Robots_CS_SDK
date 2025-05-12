e6850a09bdd69dec52d5220d1148ef0a5b54c745
Ivan <yanxiaojia@elibot.cn>
2025-05-12 09:59:13 +0800

    Improve documentation

--- source/Elite/EliteDriver.cpp
+++ source/Elite/EliteDriver.cpp
@@ -236,18 +236,18 @@ bool EliteDriver::stopControl(int wait_ms) {
     if(!impl_->reverse_server_->stopControl()) {
         return false;
     }
-    if (wait_ms > 0) {
-        auto start_time = std::chrono::steady_clock::now();
-        auto timeout = std::chrono::milliseconds(wait_ms);
-        while(impl_->script_command_server_->isRobotConnect() || impl_->reverse_server_->isRobotConnect()) {
-            auto elapsed = std::chrono::steady_clock::now() - start_time;
-            if (elapsed >= timeout) {
-                return false;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+    auto start_time = std::chrono::steady_clock::now();
+    auto timeout = std::chrono::milliseconds(wait_ms);
+    while(impl_->script_command_server_->isRobotConnect() || impl_->reverse_server_->isRobotConnect()) {
+        auto elapsed = std::chrono::steady_clock::now() - start_time;
+        if (elapsed >= timeout) {
+            return false;
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
-    return !impl_->script_command_server_->isRobotConnect();
+
+    return !isRobotConnected();
 }
 
 bool EliteDriver::writeIdle(int timeout_ms) {
